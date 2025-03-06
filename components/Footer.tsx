bfdeba97612a758a1b75861bff521d84e1109d90
Akele <achorisaac@gmail.com>
2025-03-06 15:17:23 +0100

    Version 1.0

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mt-16 py-5 text-center border-t">
+    <footer className="mt-16 py-5 text-center">
       <p className="text-gray-500">
         © 2025 Akeleojo Achor. Some not all rights reserved.
       </p>
